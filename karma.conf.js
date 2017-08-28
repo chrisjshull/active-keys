c074dc44f35a605fe1b3492a10e6a7dc95625dd2
Chris J. Shull <chris@dremio.com>
2017-08-28 15:30:16 -0700

    code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -6,22 +6,17 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'chai'],
-
 
     // list of files / patterns to load in the browser
     files: [
       {pattern: 'src/**/*.test.js', watch: false}
     ],
 
-
     // list of files to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -29,34 +24,27 @@ module.exports = function(config) {
       'src/**/*.test.js': ['webpack']
     },
 
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    reporters: ['progress', 'coverage'],
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['ChromeHeadless'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -88,6 +76,13 @@ module.exports = function(config) {
       // webpack-dev-middleware configuration
       // i. e.
       stats: 'errors-only'
+    },
+
+    coverageReporter: {
+      reporters: [
+        {type: 'text'},
+        { type: 'html'}
+      ]
     }
   });
 };
